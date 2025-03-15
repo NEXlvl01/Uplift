d3ce87c7085f19e712c9c2eb7cf44293be2b5185
Arman Thakur <122355920+NEXlvl01@users.noreply.github.com>
2025-03-15 23:45:19 +0530

    changed backend URL after production

--- client/src/axiosConfig.tsx
+++ client/src/axiosConfig.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000/";
+const API_BASE_URL = "https://uplift-backend-gamma.vercel.app/";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
